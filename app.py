d58d4630de74b72e9a01a858849d7cb333a836eb
Elaine Mari Saban Buni <rudgeremojo21@gmail.com>
2025-12-12 21:59:23 +0800

    modify config

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import pymysql
 from pymysql.err import IntegrityError
 
 app = Flask(__name__)
-mysql = MySQL()
 
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'rudge21'
 app.config['MYSQL_DATABASE_DB'] = 'api'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+
+mysql = MySQL()
+mysql.init_app(app)
 
 employee = [
     {"id": 1, "name": "John Doe", "age": "28", 'address': 'New York'},
