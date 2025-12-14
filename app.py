9aa1bd45eff315ba9fe7f140ae5e0cd179de0b33
unknown <rudgeremojo21@gmail.com>
2025-12-14 12:56:54 +0800

    modify app.py  imports

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, request, jsonify
-from flaskext.mysql import MySQL
-import pymysql
-from pymysql.err import IntegrityError
+from datetime import timedelta
+from typing import Any, Dict, Tuple
 
+from dicttoxml import dicttoxml
+from flask import Flask, jsonify, make_response, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_mysqldb import MySQL
 
+from config import apply_mysql_config
+
+app = Flask(__name__)
+apply_mysql_config(app)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
